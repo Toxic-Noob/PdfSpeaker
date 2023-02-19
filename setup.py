f89afe769fb3e1b8890ed6b5fca0d097e4b17e17
ToxicNoob <ToxicNoob.Sl4d3.Official@gmail.com>
2023-02-19 13:32:43 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+import subprocess as sb
+import platform
+import sys
+
+def getDevice():
+    system = platform.system()
+    if (system == "Linux"):
+        if hasattr(sys, "getandroidapilevel"):
+            userDevice = "Android"
+        else:
+            userDevice = "Linux"
+    elif (system == "Windows"):
+        userDevice = "Windows"
+    else:
+        print("Your Device is Unrecognized")
+        sys.exit()
+    
+    return userDevice
+
+print("[!] Stand By, Installing packages...\n")
+
+userDevice = getDevice()
+
+os.system("pip install PyPDF2")
+os.system("pip install googletrans")
+os.system("pip install pyttsx3")
+
+
+if (userDevice == "Android"):
+    if (sb.getoutput("command -v termux-tts-speak") == ""):
+        os.system("pkg install termux-api")
+        print("\n[!] Please install Termux-api apk from the link below in your device to work perfectly")
+        print("[!] The main feature requires Termux-api to be Installed")
+        print("[!] APK Link: https://f-droid.org/repo/com.termux.api_51.apk")
+        print("\n[*] Don't worry, the app is secure ;)\n")
+
+print("\n[!] Installation Complete!\n")
